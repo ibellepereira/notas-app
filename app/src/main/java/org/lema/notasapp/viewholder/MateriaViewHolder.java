6e4b1c0d72545b5bcf0be7f5a2ebbb8658a490fb
IgorRamosBR <igorrpestana@yahoo.com.br>
2016-12-19 00:07:04 -0200

    Layout boletim

--- app/src/main/java/org/lema/notasapp/viewholder/MateriaViewHolder.java
+++ app/src/main/java/org/lema/notasapp/viewholder/MateriaViewHolder.java
@@ -1,6 +1,5 @@
 package org.lema.notasapp.viewholder;
 
-import android.support.annotation.StringDef;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -19,7 +18,7 @@ public class MateriaViewHolder extends RecyclerView.ViewHolder {
     final TextView av3;
     final TextView media;
     final TextView nomeMateria;
-    final TextView codigoMateria;
+    final TextView periodoMateria;
     final TextView curso;
 
     public MateriaViewHolder(View itemView) {
@@ -30,7 +29,7 @@ public class MateriaViewHolder extends RecyclerView.ViewHolder {
         av3 = (TextView) itemView.findViewById(R.id.av3);
         media = (TextView) itemView.findViewById(R.id.media);
         nomeMateria = (TextView) itemView.findViewById(R.id.disciplina);
-        codigoMateria = (TextView) itemView.findViewById(R.id.codigoMateria);
+        periodoMateria = (TextView) itemView.findViewById(R.id.periodoMateria);
         curso = (TextView) itemView.findViewById(R.id.curso);
 
     }
@@ -41,7 +40,7 @@ public class MateriaViewHolder extends RecyclerView.ViewHolder {
         av3.setText(String.valueOf(materia.getNotaDaAv3()));
         media.setText(String.valueOf(materia.getMedia()));
         nomeMateria.setText(String.valueOf(materia.getNome()));
-        codigoMateria.setText("CC2016");
+        periodoMateria.setText(materia.getAno());
         curso.setText("Ciencia da Computacao");
     }
 }
